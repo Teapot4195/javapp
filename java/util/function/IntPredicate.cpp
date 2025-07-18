bd98aa85d95ffa7c0040fbff4bb0f6b824ae0611
Teapot4195 <teapot4195@gmail.com>
2025-07-17 21:45:51 -0400

    IntPredicate Implementation

---
+++ java/util/function/IntPredicate.cpp
@@ -0,0 +1,25 @@
+#include "IntPredicate.h"
+
+namespace java::util::function {
+    shared<IntPredicate> IntPredicate::And(const shared<IntPredicate> other) {
+        return alloc<IntPredicate>([&](const int value) {
+            return test(value) && other->test(value);
+        });
+    }
+
+    shared<IntPredicate> IntPredicate::negate() {
+        return alloc<IntPredicate>([&](const int value) {
+            return !test(value);
+        });
+    }
+
+    shared<IntPredicate> IntPredicate::Or(const shared<IntPredicate> other) {
+        return alloc<IntPredicate>([&](const int value) {
+            return test(value) || other->test(value);
+        });
+    }
+
+    bool IntPredicate::test(const int value) {
+        return inner(value);
+    }
+}
---
+++ java/util/function/IntPredicate.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <Object.h>
+
+namespace java::util::function {
+    class IntPredicate : public virtual Object {
+        std::function<bool(int)> inner;
+
+    public:
+        ~IntPredicate() override = default;
+
+        /**
+         * Implicit conversion function
+         * @tparam F function type
+         * @param func the inner function
+         */
+        template <typename F>
+        IntPredicate(F&& func) : inner(func) {}
+
+        virtual shared<IntPredicate> And(shared<IntPredicate> other);
+
+        virtual shared<IntPredicate> negate();
+
+        virtual shared<IntPredicate> Or(shared<IntPredicate> other);
+
+        virtual bool test(int value);
+    };
+
+}
