1901ca5a81a190a812dd26fa46cd9cd3a49cf085
Teapot4195 <teapot4195@gmail.com>
2025-07-18 18:24:05 -0400

    Implement Stream::Builder

--- java/util/stream/Stream.cpp
+++ java/util/stream/Stream.cpp
@@ -1,4 +1,8 @@
 #include "Stream.h"
 
 namespace java::util::stream {
+    shared<Stream::Builder> Stream::Builder::add(const shared<Object> T) {
+        accept(T);
+        return std::dynamic_pointer_cast<Builder>(shared_from_this());
+    }
 }
--- java/util/stream/Stream.h
+++ java/util/stream/Stream.h
@@ -3,10 +3,24 @@
 #include "BaseStream.h"
 #include <Object.h>
 
+#include "java/util/function/Consumer.h"
+
 namespace java::util::stream {
     class Stream : public virtual Object, public virtual BaseStream {
     public:
+        class Builder;
+
         ~Stream() override = default;
     };
 
+    class Stream::Builder : public virtual Object, public virtual function::Consumer {
+    public:
+        ~Builder() override = default;
+
+        void accept(shared<Object> T) override = 0;
+
+        virtual shared<Builder> add(shared<Object> T);
+
+        virtual shared<Stream> build() = 0;
+    };
 }
