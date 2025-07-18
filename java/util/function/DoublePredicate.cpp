1c9c35a38f30a5880a8a90b7535c375fa51744f5
Teapot4195 <teapot4195@gmail.com>
2025-07-17 22:09:08 -0400

    DoublePredicate Implementation

---
+++ java/util/function/DoublePredicate.cpp
@@ -0,0 +1,25 @@
+#include "DoublePredicate.h"
+
+namespace java::util::function {
+    shared<DoublePredicate> DoublePredicate::And(const shared<DoublePredicate> other) {
+        return alloc<DoublePredicate>([&](const double value) -> bool {
+            return test(value) && other->test(value);
+        });
+    }
+
+    shared<DoublePredicate> DoublePredicate::negate() {
+        return alloc<DoublePredicate>([&](const double value) -> bool {
+            return !test(value);
+        });
+    }
+
+    shared<DoublePredicate> DoublePredicate::Or(const shared<DoublePredicate> other) {
+        return alloc<DoublePredicate>([&](const double value) -> bool {
+            return test(value) || other->test(value);
+        });
+    }
+
+    bool DoublePredicate::test(const double value) {
+        return inner(value);
+    }
+}
---
+++ java/util/function/DoublePredicate.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <Object.h>
+
+namespace java::util::function {
+    class DoublePredicate : public virtual Object {
+        std::function<bool(double)> inner;
+
+    public:
+        ~DoublePredicate() override = default;
+
+        /**
+         * @brief Implicit conversion function
+         * @tparam F function type
+         * @param func the inner function
+         */
+        template <typename F>
+        DoublePredicate(F&& func) : inner(std::forward<F>(func)) {}
+
+        virtual shared<DoublePredicate> And(shared<DoublePredicate> other);
+
+        virtual shared<DoublePredicate> negate();
+
+        virtual shared<DoublePredicate> Or(shared<DoublePredicate> other);
+
+        virtual bool test(double value);
+    };
+
+}
