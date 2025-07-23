daa0012ef14e59f052ecd88ffebf4e8dea6e4fc9
Teapot4195 <teapot4195@gmail.com>
2025-07-23 15:01:52 -0400

    Implement DoubleStream Builder and DoubleMapMultiConsumer

--- java/util/stream/DoubleStream.cpp
+++ java/util/stream/DoubleStream.cpp
@@ -1,4 +1,8 @@
 #include "DoubleStream.h"
 
 namespace java::util::stream {
+    shared<DoubleStream::Builder> DoubleStream::Builder::add(double t) {
+        this->accept(t);
+        return std::dynamic_pointer_cast<Builder>(shared_from_this());
+    }
 }
--- java/util/stream/DoubleStream.h
+++ java/util/stream/DoubleStream.h
@@ -1,10 +1,42 @@
 #pragma once
 
 #include "BaseStream.h"
+#include "java/util/function/DoubleConsumer.h"
 
 namespace java::util::stream {
     class DoubleStream : public virtual Object, public virtual BaseStream {
     public:
         ~DoubleStream() override = default;
+
+        class DoubleMapMultiConsumer;
+        class Builder;
+    };
+
+    class DoubleStream::DoubleMapMultiConsumer : public virtual Object {
+        std::function<void(double, shared<function::DoubleConsumer>)> inner;
+
+    public:
+        ~DoubleMapMultiConsumer() override = default;
+
+        /**
+         * @brief Implicit conversion function
+         * @tparam F function type
+         * @param func the inner function
+         */
+        template <typename F>
+        DoubleMapMultiConsumer(F&& func) : inner(std::forward<F>(func)) {}
+
+        virtual void accept(double value, shared<function::DoubleConsumer> dc);
+    };
+
+    class DoubleStream::Builder : public virtual Object, public virtual function::DoubleConsumer {
+    public:
+        ~Builder() override = default;
+
+        void accept(double value) override = 0;
+
+        virtual shared<Builder> add(double t);
+
+        virtual shared<DoubleStream> build() = 0;
     };
 }
