9d0c1fbd889c250220664a0e5f1a6fb223cc68cb
Teapot4195 <teapot4195@gmail.com>
2025-08-02 07:46:11 -0400

    Implement ArrayList

---
+++ java/util/ArrayList.cpp
@@ -0,0 +1,421 @@
+#include "ArrayList.h"
+
+#include "Iterator.h"
+#include "Objects.h"
+#include "Spliterator.h"
+#include "function/Consumer.h"
+#include "function/Predicate.h"
+
+// TODO: implementor's note, we can probably slab allocate a large block of memory from a custom class, this allows
+// us to avoid a lot of alloc delete calls (probably). /shrug
+
+namespace java::util {
+    void ArrayList::copyFrom(ArrayList *other) {
+        if (other->_capacity != _capacity)
+            throw std::runtime_error("Sizes aren't equal! This is a massive implementation issue");
+
+        std::copy_n(other->_array, other->size(), _array);
+
+        _size = other->_size;
+    }
+
+    void ArrayList::checkBounds(const int index) const {
+        if (index < 0 || index >= _size)
+            throw std::runtime_error("THROW INDEXOUTOFBOUNDSEXCEPTION");
+    }
+
+    void ArrayList::resize(const int newSize) {
+        if (_array == nullptr) {
+            _array = new shared<Object>[newSize];
+            _capacity = newSize;
+            return;
+        }
+        auto* newArray = new shared<Object>[newSize];
+
+        std::copy_n(_array, _size, newArray);
+
+        delete[] _array;
+
+        _array = newArray;
+        _capacity = newSize;
+    }
+
+    ArrayList::ArrayList() : ArrayList(10) {}
+
+    ArrayList::ArrayList(const int initialCapacity) : _array(new shared<Object>[initialCapacity]), _size(0),
+        _capacity(initialCapacity) {}
+
+    ArrayList::ArrayList(const shared<Collection> &c) {
+        _array = new shared<Object>[c->size()];
+        _size = c->size();
+        _capacity = c->size();
+
+        int index = 0;
+        const auto it = c->iterator();
+        while (it->hasNext() && index < _size) {
+            _array[index++] = it->next();
+        }
+
+        if (it->hasNext() || index != _size) {
+            throw std::runtime_error("size is inaccurate?");
+        }
+    }
+
+    void ArrayList::add(const int index, const shared<Object> element) {
+        checkBounds(index);
+
+        // TODO: this is potentially 2 copies, let's make it one if possible
+        if (_size + 1 >= _capacity)
+            resize(2 * _capacity);
+
+        std::copy_backward(_array, _array + _size, _array + 1);
+
+        _array[index] = element;
+    }
+
+    bool ArrayList::add(const shared<Object> e) {
+        if (_size == _capacity) {
+            ensureCapacity(_size * 2); // double size
+        }
+        _array[_size++] = e;
+        return true;
+    }
+
+    bool ArrayList::addAll(const int index, const shared<Collection> c) {
+        checkBounds(index);
+        const auto it = c->iterator();
+
+        ensureCapacity(size() + c->size());
+        std::copy_backward(_array + index, _array + _size, _array + index + c->size());
+
+        int i = 0;
+        while (it->hasNext()) {
+            _array[index + (i++)] = it->next();
+        }
+
+        _size += c->size();
+
+        return true;
+    }
+
+    bool ArrayList::addAll(const shared<Collection> c) {
+        if (c == nullptr)
+            throw std::runtime_error("THROW NULLPOINTEREXCEPTION");
+
+        ensureCapacity(c->size());
+
+        const auto it = c->iterator();
+
+        while (it->hasNext()) {
+            add(it->next());
+        }
+
+        return true;
+    }
+
+    void ArrayList::clear() {
+        delete[] _array;
+        _array = nullptr;
+        _size = 0;
+        _capacity = 0;
+    }
+
+    std::shared_ptr<Object> ArrayList::clone() {
+        auto obj = alloc<ArrayList>(_capacity);
+        obj->copyFrom(this);
+        return obj;
+    }
+
+    bool ArrayList::contains(const shared<Object> o) {
+        for (int i = 0; i < _size; i++) {
+            if (Objects::equals(o, _array[i]))
+                return true;
+        }
+
+        return false;
+    }
+
+    void ArrayList::ensureCapacity(const int minCapacity) {
+        if (_capacity < minCapacity) {
+            resize(minCapacity);
+        }
+    }
+
+    bool ArrayList::equals(Object *obj) {
+        if (obj == this)
+            return true;
+        if (!obj->instanceof<List>())
+            return false;
+        auto* other = dynamic_cast<List *>(obj);
+        if (other->size() != _size)
+            return false;
+
+        const auto it = other->iterator();
+
+        for (int i = 0; i < _size; i++) {
+            if (!it->hasNext())
+                return false;
+            auto onext = it->next();
+            auto inext = _array[i];
+
+            if (inext == nullptr ? onext != nullptr : !inext->equals(onext))
+                return false;
+        }
+
+        if (it->hasNext())
+            return false;
+
+        return true;
+    }
+
+    void ArrayList::forEach(const shared<function::Consumer> action) {
+        for (int i = 0; i < _size; i++) {
+            action->accept(_array[i]);
+        }
+    }
+
+    shared<Object> ArrayList::get(const int index) {
+        checkBounds(index);
+        return _array[index];
+    }
+
+    int ArrayList::hashCode() {
+        int hashCode = 1;
+        const auto it = iterator();
+
+        while (it->hasNext()) {
+            auto e = it->next();
+            hashCode = 31 * hashCode + (e == nullptr ? 0 : e->hashCode());
+        }
+
+        return hashCode;
+    }
+
+    int ArrayList::indexOf(const shared<Object> o) {
+        for (int i = 0; i < _size; i++) {
+            if (Objects::equals(o, _array[i]))
+                return i;
+        }
+
+        return -1;
+    }
+
+    bool ArrayList::isEmpty() {
+        return _size == 0;
+    }
+
+    shared<Iterator> ArrayList::iterator() {
+        return AbstractList::iterator();
+    }
+
+    int ArrayList::lastIndexOf(const shared<Object> o) {
+        for (int i = _size - 1; i >= 0; i--) {
+            if (Objects::equals(o, _array[i]))
+                return i;
+        }
+
+        return -1;
+    }
+
+    shared<ListIterator> ArrayList::listIterator() {
+        return AbstractList::listIterator();
+    }
+
+    shared<ListIterator> ArrayList::listIterator(const int index) {
+        checkBounds(index);
+
+        return AbstractList::listIterator(index);
+    }
+
+    shared<Object> ArrayList::remove(const int index) {
+        checkBounds(index);
+
+        auto prev = _array[index];
+
+        std::copy(_array + index + 1, _array + _size, _array + index);
+
+        return prev;
+    }
+
+    bool ArrayList::remove(const shared<Object> o) {
+        for (int i = 0; i < _size; i++) {
+            if (Objects::equals(o, _array[i])) {
+                std::copy(_array + i + 1, _array + _size, _array + i);
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    bool ArrayList::removeAll(const shared<Collection> c) {
+        const auto it = c->iterator();
+
+        bool changed = false;
+
+        while (it->hasNext()) {
+            remove(it->next());
+            changed = true;
+        }
+
+        return changed;
+    }
+
+    bool ArrayList::removeIf(const shared<function::Predicate> filter) {
+        if (filter == nullptr)
+            throw std::runtime_error("THROW NULLPOINTEREXCEPTION");
+
+        int newSize = 0;
+        for (int i = 0, head = 0; i < _size; i++) {
+            if (!filter->test(_array[i])) {
+                newSize++;
+
+                if (head != i)
+                    _array[head++] = _array[i];
+                else
+                    head++;
+            }
+        }
+
+        std::swap(_size, newSize);
+        return _size != newSize;
+    }
+
+    void ArrayList::removeRange(const int fromIndex, const int toIndex) {
+        checkBounds(fromIndex);
+        checkBounds(toIndex);
+        if (toIndex < fromIndex)
+            throw std::runtime_error("THROW INDEXOUTOFBOUNDSEXCEPTION");
+
+        if (toIndex == fromIndex)
+            return;
+
+        const int sChange = toIndex - fromIndex;
+
+        if (toIndex != _size) {
+            // not last index, means we need to shift over
+            std::copy(_array + toIndex, _array + _size, _array + fromIndex);
+        }
+
+        _size -= sChange;
+    }
+
+    bool ArrayList::retainAll(const shared<Collection> c) {
+        if (c == nullptr)
+            throw std::runtime_error("THROW NULLPOINTEREXCEPTION");
+
+        int newSize = 0;
+        for (int i = 0, head = 0; i < _size; i++) {
+            if (c->contains(_array[i])) {
+                newSize++;
+
+                if (head != i)
+                    _array[head++] = _array[i];
+                else
+                    head++;
+            }
+        }
+
+        std::swap(_size, newSize);
+        return _size != newSize;
+    }
+
+    shared<Object> ArrayList::set(const int index, const shared<Object> element) {
+        checkBounds(index);
+        auto prev = _array[index];
+        _array[index] = element;
+        return prev;
+    }
+
+    int ArrayList::size() {
+        return _size;
+    }
+
+    class ArrayList_Spliterator_Specialization final : public virtual Object, public virtual Spliterator {
+        shared<ArrayList> parent;
+        int index = 0;
+        int limit;
+
+    public:
+        explicit ArrayList_Spliterator_Specialization(const shared<ArrayList> &parent) : parent(parent), limit(parent->size()) {}
+
+        explicit ArrayList_Spliterator_Specialization(const shared<ArrayList> &parent, const int index, const int limit)
+          : parent(parent), index(index), limit(limit) {}
+
+        int characteristics() override {
+            return SIZED | SUBSIZED | ORDERED;
+        }
+
+        long long estimateSize() override {
+            return limit - index;
+        }
+
+        bool tryAdvance(const shared<function::Consumer> action) override {
+            if (index + 1 <= limit) {
+                action->accept(parent->_array[index++]);
+                return true;
+            }
+            return false;
+        }
+
+        shared<Spliterator> trySplit() override;
+    };
+
+    shared<Spliterator> ArrayList_Spliterator_Specialization::trySplit() {
+        if (index == limit)
+            return nullptr; // nothing to split
+        if (limit - index == 1)
+            return nullptr; // can't split
+        int partition = (index + limit) / 2;
+        index = partition;
+        return alloc<ArrayList_Spliterator_Specialization>(parent, index, partition);
+    }
+
+    shared<Spliterator> ArrayList::spliterator() {
+        return alloc<ArrayList_Spliterator_Specialization>(std::dynamic_pointer_cast<ArrayList>(shared_from_this()));
+    }
+
+    shared<List> ArrayList::subList(const int fromIndex, const int toIndex) {
+        return AbstractList::subList(fromIndex, toIndex);
+    }
+
+    shared<Array<>> ArrayList::toArray() {
+        auto arr = alloc<Array<>>(_size);
+
+        std::copy_n(_array, _size, arr->data);
+        arr->length = _size;
+
+        return arr;
+    }
+
+    shared<Array<>> ArrayList::toArray(const shared<function::IntFunction> generator) {
+        return AbstractList::toArray(generator);
+    }
+
+    shared<Array<>> ArrayList::toArray(const shared<Array<>> a) {
+        auto arr = a;
+
+        if (arr->length < _size) {
+            arr->resize(_size);
+        }
+
+        std::copy_n(_array, _size, arr->data);
+
+        if (arr->length > _size) {
+            for (int i = _size; i < arr->length; i++)
+                arr->data[i] = nullptr;
+        }
+
+        return arr;
+    }
+
+    void ArrayList::trimToSize() {
+        auto* newArray = new shared<Object>[_size];
+
+        std::copy_n(_array, _size, newArray);
+
+        delete[] _array;
+
+        _array = newArray;
+    }
+}
---
+++ java/util/ArrayList.h
@@ -0,0 +1,98 @@
+#pragma once
+
+#include <Cloneable.h>
+#include "List.h"
+#include <Object.h>
+#include "AbstractList.h"
+#include "RandomAccess.h"
+#include <java/io/Serializable.h>
+
+namespace java::util {
+    class ArrayList : public virtual Object, public virtual List, public virtual RandomAccess, public virtual Cloneable,
+                      public virtual AbstractList, public virtual io::Serializable {
+        shared<Object>* _array;
+        int _size;
+        int _capacity;
+
+        void copyFrom(ArrayList *other);
+
+        void checkBounds(int index) const;
+
+        void resize(int newSize);
+
+        friend class ArrayList_Spliterator_Specialization;
+
+    public:
+        ArrayList();
+
+        explicit ArrayList(int initialCapacity);
+
+        explicit ArrayList(const shared<Collection> &c);
+
+        void add(int index, shared<Object> element) override;
+
+        bool add(shared<Object> e) override;
+
+        bool addAll(int index, shared<Collection> c) override;
+
+        bool addAll(shared<Collection> c) override;
+
+        void clear() override;
+
+        std::shared_ptr<Object> clone() override;
+
+        bool contains(shared<Object> o) override;
+
+        virtual void ensureCapacity(int minCapacity);
+
+        bool equals(Object *obj) override;
+
+        void forEach(shared<function::Consumer> action) override;
+
+        shared<Object> get(int index) override;
+
+        int hashCode() override;
+
+        int indexOf(shared<Object> o) override;
+
+        bool isEmpty() override;
+
+        shared<Iterator> iterator() override;
+
+        int lastIndexOf(shared<Object> o) override;
+
+        shared<ListIterator> listIterator() override;
+
+        shared<ListIterator> listIterator(int index) override;
+
+        shared<Object> remove(int index) override;
+
+        bool remove(shared<Object> o) override;
+
+        bool removeAll(shared<Collection> c) override;
+
+        bool removeIf(shared<function::Predicate> filter) override;
+
+    protected:
+        void removeRange(int fromIndex, int toIndex) override;
+
+    public:
+        bool retainAll(shared<Collection> c) override;
+
+        shared<Object> set(int index, shared<Object> element) override;
+
+        int size() override;
+
+        shared<Spliterator> spliterator() override;
+
+        shared<List> subList(int fromIndex, int toIndex) override;
+
+        shared<Array<>> toArray() override;
+
+        shared<Array<>> toArray(shared<function::IntFunction> generator) override;
+
+        shared<Array<>> toArray(shared<Array<>> a) override;
+
+        virtual void trimToSize();
+    };
+}
