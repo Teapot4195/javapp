c958e8dd449b40a2034d3cfecac2d4d32e8a4444
teapot4195 <teapot4195@gmail.com>
2025-11-19 21:26:53 -0500

    Implement Boolean

---
+++ src/Boolean.cpp
@@ -0,0 +1,96 @@
+#include "Boolean.h"
+
+#include <String.h>
+
+// Suppress deprecation of the constructors
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+const shared<Boolean> Boolean::FALSE = alloc<Boolean>(false), Boolean::TRUE = alloc<Boolean>(true);
+#pragma GCC diagnostic pop
+
+const shared<String> Boolean::STR_TRUE = alloc<String>("true"), Boolean::STR_FALSE = alloc<String>("false");
+
+Boolean::Boolean(bool value) : value(value) {}
+
+Boolean::Boolean(const shared<String> &s) : value(parseBoolean(s)) {}
+
+bool Boolean::booleanValue() const {
+    return value;
+}
+
+int Boolean::compare(bool x, bool y) {
+    if (x == y)
+        return 0;
+    if (!x && y)
+        return -1;
+    if (x && !y)
+        return 1;
+    std::unreachable();
+}
+
+int Boolean::compareTo(const shared<Boolean> &b) {
+    return compare(value, b->value);
+}
+
+bool Boolean::equals(Object *obj) {
+    if (!obj)
+        return false;
+    if (!obj->instanceof<Boolean>())
+        return false;
+
+    const auto* other = dynamic_cast<Boolean*>(obj);
+
+    return other->value == value;
+}
+
+int Boolean::hashCode() {
+    return hashCode(value);
+}
+
+int Boolean::hashCode(bool value) {
+    if (value)
+        return 1231;
+    return 1237;
+}
+
+bool Boolean::logicalAnd(bool a, bool b) {
+    return a && b;
+}
+
+bool Boolean::logicalOr(bool a, bool b) {
+    return a || b;
+}
+
+bool Boolean::logicalXor(bool a, bool b) {
+    return a ^ b;
+}
+
+bool Boolean::parseBoolean(const shared<String> &name) {
+    if (!name) {
+        std::string v = name->get_backing();
+        std::ranges::transform(v, v.begin(), ::tolower);
+        if (v == "true")
+            return true;
+    }
+    return false;
+}
+
+shared<String> Boolean::toString() {
+    return toString(value);
+}
+
+shared<String> Boolean::toString(bool value) {
+    if (value)
+        return STR_TRUE;
+    return STR_FALSE;
+}
+
+shared<Boolean> Boolean::valueOf(bool value) {
+    if (value)
+        return TRUE;
+    return FALSE;
+}
+
+shared<Boolean> Boolean::valueOf(const shared<String> &s) {
+    return valueOf(parseBoolean(s));
+}
---
+++ src/Boolean.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include <Object.h>
+
+class Boolean final : public virtual Object {
+    const bool value;
+
+    static const shared<String> STR_TRUE, STR_FALSE;
+
+public:
+    static const shared<Boolean> FALSE, TRUE;
+
+    [[deprecated("It is rarely appropriate to use this constructor.")]]
+    explicit Boolean(bool value);
+
+    [[deprecated("It is rarely appropriate to use this constructor.")]]
+    explicit Boolean(const shared<String>& s);
+
+    bool booleanValue() const;
+
+    static int compare(bool x, bool y);
+
+    int compareTo(const shared<Boolean>& b);
+
+    bool equals(Object *obj) override;
+
+    DEFINE_SHARED_EQUALS
+
+    // TODO: implement once properties is implemented
+    // Properties is not at all implemented lol
+    // static bool getBoolean(const shared<String>& name);
+
+    int hashCode() override;
+
+    static int hashCode(bool value);
+
+    static bool logicalAnd(bool a, bool b);
+
+    static bool logicalOr(bool a, bool b);
+
+    static bool logicalXor(bool a, bool b);
+
+    static bool parseBoolean(const shared<String>& name);
+
+    shared<String> toString() override;
+
+    static shared<String> toString(bool value);
+
+    static shared<Boolean> valueOf(bool value);
+
+    static shared<Boolean> valueOf(const shared<String>& s);
+};
